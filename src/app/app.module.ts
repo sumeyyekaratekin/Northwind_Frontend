de67ea6fa2029538b18455da3a22b51c5826fcb1
sumeyyekaratekin <sumeyyekaratekin96@gmail.com>
2021-03-07 01:30:35 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
-import { CategoryComponent } from './components/category/category.component';
-import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    CategoryComponent,
-    NaviComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
