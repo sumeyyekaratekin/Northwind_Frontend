9545590de94375d631fb468898c870e6a577c9e3
sumeyyekaratekin <sumeyyekaratekin96@gmail.com>
2021-03-10 22:25:17 +0300

    Updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'northwind';
-  user: string = 'Engin Demiroğ';
+  user: string = 'Sümeyye Karatekin';
   
 }
